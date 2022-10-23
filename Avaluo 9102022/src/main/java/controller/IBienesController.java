c2e4b1277d73cbf6efa083b10e4b546c0d8a5cf7
natyvela77 <111192890+natyvela77@users.noreply.github.com>
2022-10-23 16:22:20 -0500

    Delete Avaluo 9102022 directory

--- Avaluo 9102022/src/main/java/controller/IBienesController.java
+++
@@ -1,8 +0,0 @@
-package controller;
-
-import java.util.Map;
-
-public interface IBienesController {
-    
-    public String registerpropiedad(String idpropiedad, String username, String area, String ciudad, String estrato, String antiguedad, String tipovivienda, String habitaciones, String banos, String centrocomercial, String parque, String valorarriendo, String valorventa, String valormetro);
-}
